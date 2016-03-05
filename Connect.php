9e510fc5cf8fb1459c35d08f60cd4b50d1886cfb
brelcworks <brelcworks@gmail.com>
2016-03-05 21:42:52 +0530

    connect.php

--- Connect.php
+++ Connect.php
@@ -1,10 +1,8 @@
 <?php
-$db = parse_ini_file("set.ini");
-$user = $db['user'];
-$pass = $db['pass'];
-$name = $db['name'];
-$host = $db['host'];
-$type = $db['type'];
+$user = "afcujvrlzjemqcva";
+$pass = "BFg8WdqaarvDjTpnyxP7ojFoneT6HeB3igQG76a76eXeCkrkg7epz3Tr8ZURrty4";
+$name = "dbe891d6b4ce2a48939823a5a900b1754d";
+$host = "e891d6b4-ce2a-4893-9823-a5a900b1754d.mysql.sequelizer.com";
 
 $db1 = mysqli_connect($host, $user, $pass, $name);
 if($db1) {
